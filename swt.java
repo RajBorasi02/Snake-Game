697d871e7d71ff346032ecae1d7cc28dc50be899
RajBorasi02 <157694209+RajBorasi02@users.noreply.github.com>
2025-02-17 00:49:40 +0530

    Add files via upload

---
+++ swt.java
@@ -0,0 +1,16 @@
+class swt
+{
+	public static void main(String ar[])
+	{
+		int i,j;
+		int n=11;
+		for(i=1; i<=11; i++)
+		{
+			for(j=1; j<=11; j++)
+			{
+				System.out.println((i+j)+"/t");
+			}
+			System.out.print();
+		}
+	}
+}
